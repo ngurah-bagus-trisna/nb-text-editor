b7153479778c6b7b374fa889d1836c9b0bdf8979
ngurah <ngurah@btech.id>
2024-02-07 10:39:04 +0700

    Add folder view, and some text editor

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,20 +23,37 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         navBar = New MenuStrip()
+        OpenFolderToolStripMenuItem = New ToolStripMenuItem()
         FileToolStripMenuItem = New ToolStripMenuItem()
         SaveToolStripMenuItem = New ToolStripMenuItem()
         SaveAsToolStripMenuItem = New ToolStripMenuItem()
+        openFolder = New FolderBrowserDialog()
+        openFile = New OpenFileDialog()
+        SplitContainer1 = New SplitContainer()
+        folderView = New TreeView()
+        RichTextBox1 = New RichTextBox()
+        TextBox1 = New TextBox()
         navBar.SuspendLayout()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.Panel1.SuspendLayout()
+        SplitContainer1.Panel2.SuspendLayout()
+        SplitContainer1.SuspendLayout()
         SuspendLayout()
         ' 
         ' navBar
         ' 
-        navBar.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, SaveToolStripMenuItem, SaveAsToolStripMenuItem})
+        navBar.Items.AddRange(New ToolStripItem() {OpenFolderToolStripMenuItem, FileToolStripMenuItem, SaveToolStripMenuItem, SaveAsToolStripMenuItem})
         navBar.Location = New Point(0, 0)
         navBar.Name = "navBar"
         navBar.Size = New Size(952, 24)
         navBar.TabIndex = 0
         navBar.Text = "MenuStrip1"
+        ' 
+        ' OpenFolderToolStripMenuItem
+        ' 
+        OpenFolderToolStripMenuItem.Name = "OpenFolderToolStripMenuItem"
+        OpenFolderToolStripMenuItem.Size = New Size(84, 20)
+        OpenFolderToolStripMenuItem.Text = "Open Folder"
         ' 
         ' FileToolStripMenuItem
         ' 
@@ -56,11 +73,57 @@ Partial Class Form1
         SaveAsToolStripMenuItem.Size = New Size(59, 20)
         SaveAsToolStripMenuItem.Text = "Save As"
         ' 
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.Location = New Point(0, 24)
+        SplitContainer1.Name = "SplitContainer1"
+        ' 
+        ' SplitContainer1.Panel1
+        ' 
+        SplitContainer1.Panel1.Controls.Add(folderView)
+        ' 
+        ' SplitContainer1.Panel2
+        ' 
+        SplitContainer1.Panel2.Controls.Add(RichTextBox1)
+        SplitContainer1.Panel2.Controls.Add(TextBox1)
+        SplitContainer1.Size = New Size(952, 545)
+        SplitContainer1.SplitterDistance = 317
+        SplitContainer1.TabIndex = 1
+        ' 
+        ' folderView
+        ' 
+        folderView.Dock = DockStyle.Fill
+        folderView.Location = New Point(0, 0)
+        folderView.Name = "folderView"
+        folderView.Size = New Size(317, 545)
+        folderView.TabIndex = 0
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.AcceptsTab = True
+        RichTextBox1.Dock = DockStyle.Fill
+        RichTextBox1.Font = New Font("Consolas", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox1.Location = New Point(0, 0)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(631, 545)
+        RichTextBox1.TabIndex = 1
+        RichTextBox1.Text = ""
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(2, 3)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(616, 542)
+        TextBox1.TabIndex = 0
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(952, 569)
+        Controls.Add(SplitContainer1)
         Controls.Add(navBar)
         Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         MainMenuStrip = navBar
@@ -69,6 +132,11 @@ Partial Class Form1
         Text = "NB Text Editor"
         navBar.ResumeLayout(False)
         navBar.PerformLayout()
+        SplitContainer1.Panel1.ResumeLayout(False)
+        SplitContainer1.Panel2.ResumeLayout(False)
+        SplitContainer1.Panel2.PerformLayout()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -77,5 +145,12 @@ Partial Class Form1
     Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents openFolder As FolderBrowserDialog
+    Friend WithEvents OpenFolderToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents openFile As OpenFileDialog
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents folderView As TreeView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -2,12 +2,16 @@
     Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles navBar.ItemClicked
 
     End Sub
-
+    Private Sub OpenFolderToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenFolderToolStripMenuItem.Click
+        openFolder.ShowDialog()
+    End Sub
     Private Sub FileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FileToolStripMenuItem.Click
-
+        openFile.ShowDialog()
     End Sub
 
     Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
 
     End Sub
+
+
 End Class
